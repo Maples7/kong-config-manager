cf1469d1c3436091beed344a48b023c2ce6e063e
Maples7 <maples7@163.com>
2017-08-07 16:08:57 +0800

    rm R_OK too

--- utils/get_config_path.js
+++ utils/get_config_path.js
@@ -6,13 +6,13 @@ module.exports = function getConfigPath(filePath) {
   const ret = getAbsolutePath(filePath);
 
   if (fs.existsSync(ret)) {
-    try {
-      fs.accessSync(ret, fs.constants.R_OK);
-    } catch (e) {
-      exit(
-        `the program doesn't have READ permission of config file ${filePath}: ${e}`
-      );
-    }
+    // try {
+    //   fs.accessSync(ret, fs.constants.R_OK);
+    // } catch (e) {
+    //   exit(
+    //     `the program doesn't have READ permission of config file ${filePath}: ${e}`
+    //   );
+    // }
 
     return ret;
   } else {
