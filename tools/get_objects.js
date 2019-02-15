4beb9f72cd9f94195ca2d65bcd35e87042d51237
Wagner Spirigoni <wagner.spirigoni@mercadobitcoin.com.br>
2019-02-15 14:44:09 -0200

    adding support for 0.33 and 0.34

--- tools/get_objects.js
+++ tools/get_objects.js
@@ -7,11 +7,20 @@ const ENUMS = require('../enums');
 module.exports = function getObjects(url) {
   return rp(url).then(body => {
     const res = JSON.parse(body);
-    const version = res.version;
+    var version;
+    if (res.version.endsWith("enterprise-edition")) {
+        version = res.version.replace("-enterprise-edition", ".0");
+    } else {
+        version = res.version;
+    }
     return semver.gte(version, '0.11.0')
       ? semver.gte(version, '0.13.0')
-        ? semver.gte(version, '0.14.0')
-          ? ENUMS.OBJECTS['0.14.x']
+	? semver.gte(version, '0.33.0')
+          ? semver.gte(version, '0.14.0')
+	    ? semver.gte(version, '0.34.0')
+              ? ENUMS.OBJECTS['0.14.x']
+              ? ENUMS.OBJECTS['0.14.x']
+	    : ENUMS.OBJECTS['0.13.x']
           : ENUMS.OBJECTS['0.13.x']
         : ENUMS.OBJECTS['0.11.x']
       : ENUMS.OBJECTS['0.10.x'];
